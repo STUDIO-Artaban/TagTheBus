d5c6c0394a4d57966b0343e2de1bf6c438f2f2a5
v-pascal <pascal.viguie@studio-artaban.com>
2015-12-15 15:51:22 +0100

    Ajout de la version iOS

---
+++ iOS/TagTheBus/TagTheBus/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  TagTheBus
+//
+//  Created by Pascal Viguié on 25/11/2015.
+//  Copyright (c) 2015 STUDIO Artaban. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ iOS/TagTheBus/TagTheBus/PhotosCell.m
@@ -0,0 +1,23 @@
+//
+//  PhotosCell.m
+//  TagTheBus
+//
+//  Created by Pascal Viguié on 07/12/2015.
+//  Copyright © 2015 STUDIO Artaban. All rights reserved.
+//
+
+#import "PhotosCell.h"
+
+@implementation PhotosCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+    
+    // Configure the view for the selected state
+}
+
+@end
